8ae7f2ec73e2a4e07c4bcb6f3ff044f1e4341d21
revanth.sreekumar <revanth.sreekumar@IN-LP-237.INTERNAL.INNOVATURELABS.COM>
2023-11-10 12:10:45 +0530

    test suite added

---
+++ Test Suites/New Test Suite 001.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>New Test Suite 001</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>f3a29418-da5d-4ccb-83b1-081713de186a</testSuiteGuid>
+</TestSuiteEntity>
